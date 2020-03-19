a7d2a64db5177414af96af978057d9dd241e63aa
tuwenjun <tuwenjun@wudi360.com>
2020-03-19 08:52:57 +0800

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command('bill:generate')
+            ->runInBackground()
+            ->monthlyOn(1, '00:50');
     }
 
     /**
