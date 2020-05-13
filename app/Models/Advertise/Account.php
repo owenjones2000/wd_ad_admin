52b1858554413294b761e87e2f08c645cebc0bb6
daizaijun <daizaijun@wudi360.com>
2020-05-13 15:44:34 +0800

    修改账单数据生成源为月度安装表

--- app/Models/Advertise/Account.php
+++ app/Models/Advertise/Account.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models\Advertise;
 
 use Illuminate\Database\Eloquent\Model;
@@ -15,7 +16,7 @@ class Account extends Model
     protected $table = 'a_users';
 
     protected $hidden = ['name', 'password_hash', 'remember_token', 'username', 'pivot',
-        'main_user_id', 'deleted_at'];
+                         'main_user_id', 'deleted_at'];
     protected $fillable = ['username', 'email', 'realname'];
 
     /**
@@ -24,8 +25,9 @@ class Account extends Model
      * @param $params
      * @return mixed
      */
-    public static function Make($params){
-        $account = DB::transaction(function () use($params) {
+    public static function Make($params)
+    {
+        $account = DB::transaction(function () use ($params) {
             if (empty($params['id'])) {
                 $account = new Account();
                 $account['status'] = true;
@@ -36,25 +38,25 @@ class Account extends Model
                 ])->firstOrFail();
             }
 
-            if (!empty($params['password'])){
+            if (!empty($params['password'])) {
                 $account['password_hash'] = Hash::make($params['password']);
             }
 
             $account->fill($params);
             $account->saveOrFail();
 
-            if(empty($params['id'])){
+            if (empty($params['id'])) {
                 $permission_keys = UaPermission::query()->pluck('id')->toArray();
                 $permissions = array_fill_keys(
                     $permission_keys,
                     [
                         'main_user_id' => $account['id'],
-                        'model_type' => 'App\User',
+                        'model_type'   => 'App\User',
                     ]
                 );
                 $account->permissions()->sync($permissions);
             }
-            
+
             return $account;
         }, 3);
 
@@ -65,8 +67,9 @@ class Account extends Model
      * 启用
      * @throws \Throwable
      */
-    public function enable(){
-        if(!$this->status){
+    public function enable()
+    {
+        if (!$this->status) {
             $this->status = true;
             $this->saveOrFail();
         }
@@ -76,8 +79,9 @@ class Account extends Model
      * 停用
      * @throws \Throwable
      */
-    public function disable(){
-        if($this->status){
+    public function disable()
+    {
+        if ($this->status) {
             $this->status = false;
             $this->saveOrFail();
         }
@@ -87,8 +91,9 @@ class Account extends Model
      * 启用广告投放
      * @throws \Throwable
      */
-    public function enableAdvertising(){
-        if(!$this->isAdvertiseEnabled){
+    public function enableAdvertising()
+    {
+        if (!$this->isAdvertiseEnabled) {
             $this->isAdvertiseEnabled = true;
             $this->saveOrFail();
         }
@@ -98,8 +103,9 @@ class Account extends Model
      * 停用广告投放
      * @throws \Throwable
      */
-    public function disableAdvertising(){
-        if($this->isAdvertiseEnabled){
+    public function disableAdvertising()
+    {
+        if ($this->isAdvertiseEnabled) {
             $this->isAdvertiseEnabled = false;
             $this->saveOrFail();
         }
@@ -109,8 +115,9 @@ class Account extends Model
      * 启用广告变现
      * @throws \Throwable
      */
-    public function enablePublishing(){
-        if(!$this->isPublishEnabled){
+    public function enablePublishing()
+    {
+        if (!$this->isPublishEnabled) {
             $this->isPublishEnabled = true;
             $this->saveOrFail();
         }
@@ -120,8 +127,9 @@ class Account extends Model
      * 停用广告变现
      * @throws \Throwable
      */
-    public function disablePublishing(){
-        if($this->isPublishEnabled){
+    public function disablePublishing()
+    {
+        if ($this->isPublishEnabled) {
             $this->isPublishEnabled = false;
             $this->saveOrFail();
         }
@@ -132,12 +140,13 @@ class Account extends Model
      *
      * @param $params
      */
-    public function setBill($params){
+    public function setBill($params)
+    {
         $this->bill()->updateOrCreate(
             [],
             [
                 'address' => Arr::get($params, 'address', ''),
-                'phone' => Arr::get($params, 'phone', ''),
+                'phone'   => Arr::get($params, 'phone', ''),
             ]
         );
     }
@@ -145,51 +154,58 @@ class Account extends Model
     /**
      * 生成账单
      */
-    public function generateBill(){
+    public function generateBill()
+    {
         $last_month_timestamp = strtotime('-1 month');
         $start_date = date('Y-m-01', $last_month_timestamp);
         $end_date = date('Y-m-t', $last_month_timestamp);
         $due_date = date('Y-m-t');
-        $fee_amount_query = Install::multiTableQuery(function($query){
-            return $query->select(['spend'])->whereIn('app_id', $this->apps()->select('id')->getQuery());
-        }, $start_date, $end_date);
+//        $fee_amount_query = Install::multiTableQuery(function($query){
+//            return $query->select(['spend'])->whereIn('app_id', $this->apps()->select('id')->getQuery());
+//        }, $start_date, $end_date);
+        $table = 'y_installations_' . date('Ym', $last_month_timestamp);
+        $fee_amount_query = Install::query()->from($table)->whereBetween('created_at', [$start_date, $end_date]);
         $fee_amount = $fee_amount_query->sum('spend');
-        if($fee_amount > 0){
-            DB::transaction(function() use($start_date, $end_date, $fee_amount, $due_date, $last_month_timestamp){
+        if ($fee_amount > 0) {
+            DB::transaction(function () use ($start_date, $end_date, $fee_amount, $due_date, $last_month_timestamp) {
                 $bill = $this->bills()->firstOrNew(
                     [
                         'start_date' => $start_date,
-                        'end_date' =>$end_date,
+                        'end_date'   => $end_date,
                     ],
                     [
                         'fee_amount' => $fee_amount,
-                        'due_date' => $due_date,
-                        'paid_at' => $fee_amount > 0 ? null : Carbon::now()
+                        'due_date'   => $due_date,
+                        'paid_at'    => $fee_amount > 0 ? null : Carbon::now()
                     ]
                 );
-                if(empty($bill['id']) || empty($bill['invoice_id'])){
+                if (empty($bill['id']) || empty($bill['invoice_id'])) {
                     $bill['invoice_id'] = 'wudiads-'
-                        .date('Ym', $last_month_timestamp)
-                        .'-'.date('md').str_pad(rand(1,999), 3, '0', STR_PAD_LEFT)
-                    ;
+                        . date('Ym', $last_month_timestamp)
+                        . '-' . date('md') . str_pad(rand(1, 999), 3, '0', STR_PAD_LEFT);
                 }
                 $bill->saveOrFail();
             }, 3);
         }
     }
 
-    public function apps(){
+    public function apps()
+    {
         return $this->hasMany(App::class, 'main_user_id', 'id');
     }
 
-    public function bill(){
+    public function bill()
+    {
         return $this->hasOne(BillSet::class, 'id', 'id');
     }
-    public function bills(){
+
+    public function bills()
+    {
         return $this->hasMany(Bill::class, 'main_user_id', 'id');
     }
 
-    public function children(){
+    public function children()
+    {
         return $this->hasMany(Account::class, 'main_user_id', 'id');
     }
 
@@ -197,20 +213,22 @@ class Account extends Model
      * 广告人员
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function advertisers(){
+    public function advertisers()
+    {
         return $this->belongsToMany(Account::class, 'a_users_advertiser',
-            'main_user_id','advertiser_user_id',
+            'main_user_id', 'advertiser_user_id',
             'id', 'id'
         );
     }
 
-    public function permissions($main_user_id = null){
+    public function permissions($main_user_id = null)
+    {
         $query = $this->belongsToMany(
             UaPermission::class,
             'ua_model_has_permissions',
             'model_id',
             'permission_id');
-        if($main_user_id){
+        if ($main_user_id) {
             $query->wherePivot('main_user_id', $main_user_id);
         }
         return $query;
@@ -219,7 +237,7 @@ class Account extends Model
     public static function rules($request_params = [])
     {
         return [
-            'email' => 'required|email|unique:a_users,email,'.($request_params['id']??''),
+            'email'    => 'required|email|unique:a_users,email,' . ($request_params['id'] ?? ''),
             'password' => empty($request_params['id']) ?
                 'required|min:2|max:14' : 'min:2|max:14'
         ];
