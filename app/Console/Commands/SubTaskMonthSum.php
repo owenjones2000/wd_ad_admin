90152ee963cf689cbb10840061e8ac8b504a5d4a
zhongixn <zhongxing@wudi360.com>
2020-05-20 19:20:03 +0800

    subtask

---
+++ app/Console/Commands/SubTaskMonthSum.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Advertise\Account;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class SubTaskMonthSum extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'subtask';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'generate subtask';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $lastmonthday = Carbon::now()->subMonth()->lastOfMonth()->format('Ymd');
+        $firstmonthday = Carbon::now()->subMonth()->firstOfMonth()->format('Ymd');
+        for ($i= $firstmonthday; $i <= $lastmonthday; $i++) { 
+            $tableName = 'z_sub_tasks_'.$i;
+            $storeName = 'y_sub_tasks_'.Carbon::now()->subMonth()->format('Ym');
+            $templateName = 'zz_sub_tasks';
+            if (Schema::connection('mysql')->hasTable($storeName) == false) {
+                DB::connection()->statement("create table $storeName like $templateName");
+            }
+            DB::connection()->statement("INSERT INTO $storeName SELECT * FROM $tableName");
+        }
+    }
+}
