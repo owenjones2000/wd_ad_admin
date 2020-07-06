7bd0f7410ae34a3530776afd99f1e7446d7ee05c
zhongixn <zhongxing@wudi360.com>
2020-07-06 16:37:22 +0800

    fix tag

--- app/Http/Controllers/Api/AudienceController.php
+++ app/Http/Controllers/Api/AudienceController.php
@@ -270,7 +270,8 @@ class AudienceController extends Controller
             $idfas = Idfa::where('tag_id', $id)->chunk(10000, function($idfas) use ($newappIds){
                 $redisValue = $idfas->pluck('idfa');
                 foreach ($newappIds as $key => $id) {
-                    $redisRes = Redis::connection('default')->sadd('app_audience_blocklist_' . $id, ...$redisValue);
+                    // $redisRes = Redis::connection('default')->sadd('app_audience_blocklist_' . $id, ...$redisValue);
+                    $redisRes = Redis::connection('feature')->sadd('app_audience_blocklist_' . $id, ...$redisValue);
                     Log::info('redisres  ' . $redisRes. '   appid '. $id);
                 }
             });
