2ff7632a6ea73720a0061752ba7e7a4d1dc63f2a
zhongixn <zhongxing@wudi360.com>
2020-07-27 09:31:00 +0800

    operation

---
+++ app/Laravue/Models/OperationLog.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Laravue\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OperationLog extends Model
+{
+    protected $table = 'admin_operation_log';
+    protected $connection = 'mysql_system';
+    protected $fillable = ['user_id', 'path', 'method', 'ip', 'input'];
+
+    public static $methodColors = [
+        'GET'    => 'primary',
+        'POST'   => 'success',
+        'PUT'    => 'blue',
+        'DELETE' => 'danger',
+    ];
+
+    public static $methods = [
+        'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH',
+        'LINK', 'UNLINK', 'COPY', 'HEAD', 'PURGE',
+    ];
+
+    /**
+     * Log belongs to users.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ config/operationlog.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'allowed_methods' => ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH'],
+
+    'secret_fields' => [
+        'password',
+        'password_confirmation',
+    ],
+
+    // Routes that will not log to database.
+    // All method to path like: auth/logs/*/edit
+    // or specific method to path like: get:auth/logs.
+    'except' => [
+        'auth/logs*',
+    ],
+];
