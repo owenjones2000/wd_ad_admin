d97a8eaee92bd92d1273b417efa10a3e82364e7f
zhongixn <zhongxing@wudi360.com>
2020-08-18 17:24:53 +0800

    credit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\AccountCredit;
 use App\Console\Commands\ChannelCpmTj;
 use App\Console\Commands\DateStatis;
 use App\Console\Commands\SubTaskMonthSum;
