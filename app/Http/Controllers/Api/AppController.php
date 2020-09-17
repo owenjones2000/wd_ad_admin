3c170c76c20956c1be54c871fac23c288d639f24
zhongixn <zhongxing@wudi360.com>
2020-09-17 09:50:30 +0800

    opt app cam

--- app/Http/Controllers/Api/AppController.php
+++ app/Http/Controllers/Api/AppController.php
@@ -280,7 +280,8 @@ class AppController extends Controller
         if (!empty($order_by_ids)) {
             $channel_query->orderByRaw(DB::raw("FIELD(id,{$order_by_ids}) desc"));
         }
-        $channel_list = $channel_query->orderBy($request->get('field', 'name'), $request->get('order', 'desc'))
+        $channel_list = $channel_query
+            ->orderBy('id', 'desc')
             ->paginate($request->get('limit', 30));
 
         foreach ($channel_list as $index => &$channel) {
@@ -296,6 +297,8 @@ class AppController extends Controller
         $range_date = $request->get('daterange');
         $start_date = date('Ymd', strtotime($range_date[0] ?? 'now'));
         $end_date = date('Ymd', strtotime($range_date[1] ?? 'now'));
+        $order_by = explode('.', $request->get('field', 'status'));
+        $order_sort = $request->get('order', 'desc');
 
         $campaign_base_query = Campaign::query();
         if (!empty($request->get('keyword'))) {
@@ -328,6 +331,9 @@ class AppController extends Controller
             'campaign_id',
         ]);
         $advertise_kpi_query->groupBy('campaign_id');
+        if ($order_by[0] === 'kpi' && isset($order_by[1])) {
+            $advertise_kpi_query->orderBy($order_by[1], $order_sort);
+        }
 
         $advertise_kpi_list = $advertise_kpi_query
             ->orderBy('spend', 'desc')
@@ -342,7 +348,8 @@ class AppController extends Controller
         if (!empty($order_by_ids)) {
             $campaign_query->orderByRaw(DB::raw("FIELD(id,{$order_by_ids}) desc"));
         }
-        $channel_list = $campaign_query->orderBy($request->get('field', 'name'), $request->get('order', 'desc'))
+        $channel_list = $campaign_query
+            ->orderBy('id', 'desc')
             ->paginate($request->get('limit', 30));
 
         foreach ($channel_list as $index => &$channel) {
