ffc5aba9cff87a1be70ce1b76229289f8378a0b9
zhongixn <zhongxing@wudi360.com>
2021-04-16 14:01:23 +0800

    修改invoice

--- resources/views/bill/invoice.blade.php
+++ resources/views/bill/invoice.blade.php
@@ -37,10 +37,10 @@
     <div style="height: 10px; background: #f6993f;" ></div>
     <div>
         <h3 style="font-size: 1.38125em; color: #666666;">
-            GOOD LUCK STUDIO LIMITED
+            ESSENTIALS TECH LIMITED
         </h3>
         <p style=" font-size: 0.8125em; color: #666666;">
-            Mandar House, 3rd Floor P.O.Box 2196,Johnson’s Ghut Tortola,British Virgin Islands
+            Unit D, 16/F, One Capital Place, 18 Luard Road, Wan Chai, Hong Kong
         </p>
         @if($billAdr)
         <h3 style="font-size: 1.38125em; color: #666666;">
@@ -73,7 +73,7 @@
                 {{ $bill['account']['realname'] }}
             </td>
             <td>
-                <p>GOOD LUCK STUDIO LIMITED</p>
+                <p>ESSENTIALS TECH LIMITED</p>
             </td>
             <td>
                 <p>{{ $bill['invoice_id'] }}</p>
@@ -160,25 +160,25 @@
             <th style="">Payment Info:</th>
         </tr>
         <tr class="odd-row">
-            <td>ACCOUNT NAME: GOOD LUCK STUDIO LIMITED </td>
+            <td>ACCOUNT NAME : ESSENTIALS TECH LIMITED </td>
         </tr>
         <tr class="odd-row">
-            <td>ACCOUNT NUMBER :716-113721-8 (USD)</td>
+            <td>ACCOUNT NUMBER : 012-805-92-38963-8</td>
         </tr>
         <tr class="">
-            <td>BANK NAME:   OCBC BANK (M) BERHAD</td>
+            <td>BANK NAME : BANK OF CHINA (HONG KONG) LIMITED</td>
         </tr>
         <tr class="odd-row">
-            <td>OCBC BANK SWIFT CODE: OCBCMYKL</td>
+            <td>SWIFT CODE : BKCHHKHH or BKCHHKHHXXX</td>
         </tr>
-        <tr class="">
+        {{-- <tr class="">
             <td>OCBC BRANCH CODE: First 3 digits of your account number</td>
-        </tr>
+        </tr> --}}
         <tr class="odd-row">
-            <td>Bank address:MENARA OCBC FLOOR 11 18 JALAN TUN PERAK KUALA LUMPUR 50500</td>
+            <td>Bank address : Shop 2103, Level 2, Ma On Shan Plaza, Sai Sha Road, Ma On Shan, New Territories</td>
         </tr>
         <tr class="">
-            <td>Recipient's address:Mandar House, 3rd Floor P.O.Box 2196,Johnson’s Ghut Tortola,British Virgin Islands</td>
+            <td>Recipient's address : Unit D, 16/F, One Capital Place, 18 Luard Road, Wan Chai, Hong Kong</td>
         </tr>
     </table>
     <p>
